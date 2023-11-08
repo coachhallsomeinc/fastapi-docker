e4ea75f21331cbcb20a30b3b5b84fc225c38f5dc
coachhallsomeinc <justin.hall@student.awesomeinc.org>
2023-11-08 06:45:11 +0000

    added friends and enemies to hero obj

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session, aliased, joinedload
 from models import Hero, Ability, AbilityType, Relationship, RelationshipType
 from schemas import HeroModel, AbilityModel, AbilityTypeModel, RelationshipModel, RelationshipTypeModel
-from sqlalchemy import and_
+from sqlalchemy import and_, or_
 
 def create_hero(db: Session, hero: HeroModel):
     db_hero = Hero(name=hero.name, biography=hero.biography, about_me=hero.about_me, image_url=hero.image_url | None )
@@ -52,46 +52,44 @@ def get_heroes(db: Session):
         # Query Friends and Enemies separately and populate the Hero models
         friends = (
             db.query(Hero, FriendAlias)
-            .join(FriendAlias, and_(Hero.id == hid,
+            .join(FriendAlias, and_(Hero.id == FriendAlias.hero1_id,
                                     FriendAlias.relationship_type_id == FRIENDS_TYPE_ID))
             .join(RelationshipType, FriendAlias.relationship_type_id == RelationshipType.id)
+            .filter(or_(FriendAlias.hero1_id == hid, FriendAlias.hero2_id == hid))
             .all()
         )
-    print(friends)
-    return friends
-        # for friend in friends:
-        #     friend_model = RelationshipModel(
-        #         id=friend.id,
-        #         hero1_id=friend.hero1_id,
-        #         hero2_id=friend.hero2_id,
-        #         relationship_type_id=friend.relationship_type_id,
-        #         relationship_type=RelationshipTypeModel(id=friend.relationship_type_id, name=friend.relationship_type.name)
-        #     )
-        #     heroes[hid].friends.append(friend_model)
+        
+        for hero, friend in friends:
+            friend_model = RelationshipModel(
+                id=friend.id,
+                hero1_id=friend.hero1_id,
+                hero2_id=friend.hero2_id,
+                relationship_type_id=friend.relationship_type_id,
+                relationship_type=RelationshipTypeModel(id=friend.relationship_type_id, name=friend.relationship_type.name)
+            )
+            heroes[hid].friends.append(friend_model)
 
-        # enemies = (
-        #     db.query(Hero, EnemyAlias, RelationshipType)
-        #     .join(EnemyAlias, and_(Hero.id == EnemyAlias.hero1_id,
-        #                         EnemyAlias.relationship_type_id == ENEMIES_TYPE_ID))
-        #     .join(RelationshipType, EnemyAlias.relationship_type_id == RelationshipType.id)
-        #     .all()
-        # )
+        enemies = (
+            db.query(Hero, EnemyAlias)
+            .join(EnemyAlias, and_(Hero.id == EnemyAlias.hero1_id,
+                                EnemyAlias.relationship_type_id == ENEMIES_TYPE_ID))
+            .join(RelationshipType, EnemyAlias.relationship_type_id == RelationshipType.id)
+            .filter(or_(EnemyAlias.hero1_id == hid, EnemyAlias.hero2_id == hid))
+            .all()
+        )
 
-        # for enemy, relationship_type in enemies:
-        #     relationship_type_model = RelationshipTypeModel(
-        #         id=enemy.relationship_type.id,
-        #         name=enemy.relationship_type.name
-        #     )
-        #     enemy_model = RelationshipModel(
-        #         id=enemy.id,
-        #         hero1_id=enemy.hero1_id,
-        #         hero2_id=enemy.hero2_id,
-        #         relationship_type_id=enemy.relationship_type_id,
-        #         relationship_type=relationship_type_model  # Include the RelationshipTypeModel instance
-        #     )
-        #     # Append the enemy_model to the corresponding hero's enemies list
-        #     heroes[hero.id].enemies.append(enemy_model)
+        for hero, enemy in enemies:
+            enemy_model = RelationshipModel(
+                id=enemy.id,
+                hero1_id=enemy.hero1_id,
+                hero2_id=enemy.hero2_id,
+                relationship_type_id=enemy.relationship_type_id,
+                relationship_type=RelationshipTypeModel(id=enemy.relationship_type_id, name=enemy.relationship_type.name)
+            )
+            # Append the enemy_model to the corresponding hero's enemies list
+            heroes[hid].enemies.append(enemy_model)
 
     # Convert the dictionary to a list of HeroModel instances
+
     hero_models = list(heroes.values())
     return hero_models
